84ba4a725b0f466e56c581821b468a3397eb39c3
frizzly-mika <micael.augustons@hotmail.com>
2023-06-05 18:03:46 -0300

    Projeto Conta Bancária - Parte 2

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,13 +1,37 @@
 package conta;
 
 import java.util.Scanner;
-
+import conta.model.Conta;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
 import conta.util.Cores;
 
 public class Menu {
 
 	public static void main(String[] args) {
+		
+		Conta c1 = new Conta(1, 123, 1, "Adriana", 13000.0f);
+		c1.visualizar();
+		c1.sacar(1200.0f);
+		c1.visualizar();
+		c1.depositar(5000.0f);
+		c1.visualizar();
 
+		ContaCorrente cc1 = new ContaCorrente(2, 123, 1, "Mariana", 15000.0f, 1000.0f);
+		cc1.visualizar();
+		cc1.sacar(1200.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);
+		cc1.visualizar();
+		
+		ContaPoupanca cp1 = new ContaPoupanca(3, 123, 2, "Vicktor", 11000.0f, 15);
+		cp1.visualizar();
+		cp1.sacar(1000.0f);
+		cp1.visualizar();
+		cp1.depositar(5000.0f);
+		cp1.visualizar();
+		
+		
 		Scanner leia = new Scanner(System.in);
 		int opcao;
 				
---
+++ src/conta/model/ContaPoupanca.java
@@ -0,0 +1,28 @@
+package conta.model;
+
+public class ContaPoupanca extends Conta{
+	
+	private int aniversário;
+
+	public ContaPoupanca(int numero, int agencia, int tipo, String titular, float saldo, int aniversário) {
+		super(numero, agencia, tipo, titular, saldo);
+		this.aniversário = aniversário;
+	}
+
+	public int getAniversário() {
+		return aniversário;
+	}
+
+	public void setAniversário(int aniversário) {
+		this.aniversário = aniversário;
+	}
+
+	@Override
+	public void visualizar() {
+		super.visualizar();
+		System.out.println("    |     Aniversário da conta: " + this.aniversário + "               |    ");
+	}
+	
+	
+
+}
