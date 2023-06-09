20dfc4da1cce283ff235db7d221a1e9e9c10fe74
frizzly-mika <micael.augustons@hotmail.com>
2023-06-09 18:14:20 -0300

    Projeto Conta Bancária - Parte 4 - Tratamento de Erros e Métodos CRUD

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,6 +1,10 @@
 package conta;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
+import conta.controller.ContaController;
 import conta.model.Conta;
 import conta.model.ContaCorrente;
 import conta.model.ContaPoupanca;
@@ -10,37 +14,36 @@ public class Menu {
 
 	public static void main(String[] args) {
 
-		ContaCorrente cc1 = new ContaCorrente(2, 123, 1, "Mariana", 15000.0f, 1000.0f);
-		cc1.visualizar();
-		cc1.sacar(1200.0f);
-		cc1.visualizar();
-		cc1.depositar(5000.0f);
-		cc1.visualizar();
-		
-		ContaPoupanca cp1 = new ContaPoupanca(3, 123, 2, "Vicktor", 11000.0f, 15);
-		cp1.visualizar();
-		cp1.sacar(1000.0f);
-		cp1.visualizar();
-		cp1.depositar(5000.0f);
-		cp1.visualizar();
-		
-		
+		ContaController contas = new ContaController();
+
 		Scanner leia = new Scanner(System.in);
-		int opcao;
-				
-		while(true) { 
+		int opcao, numero, agencia, tipo, aniversario;
+		String titular;
+		float saldo, limite;
+		
+		System.out.println("\nCriar contas\n");
+		
+		ContaCorrente cc1 = new ContaCorrente(contas.gerarNumero(), 123, 1, "João da Silva", 1000f, 100.0f);
+		contas.cadastrar(cc1);
+		ContaCorrente cc2 = new ContaCorrente(contas.gerarNumero(), 124, 1, "Maria da Silva", 2000f, 100.0f);
+		contas.cadastrar(cc1);
+		ContaPoupanca cp1 = new ContaPoupanca(contas.gerarNumero(), 125, 2, "Mariana dos Santos", 4000f, 12);
+		contas.cadastrar(cp1);
+		ContaPoupanca cp2 = new ContaPoupanca(contas.gerarNumero(), 126, 2, "Juliana Ramos", 8000f, 15);
+		contas.cadastrar(cp2);
+		
+		contas.listarTodas();
+
+		while (true) {
 
 			System.out.println(Cores.TEXT_BLACK + Cores.ANSI_BLUE_BACKGROUND
-			                 + "                                                      ");
+					+ "                                                      ");
 			System.out.println("       ________________________________________       ");
-			System.out.println(Cores.TEXT_BLUE_BRIGHT + Cores.ANSI_BLUE_BACKGROUND +
-							   "     ♦                  " 
-			+ Cores.TEXT_RED_BRIGHT + Cores.ANSI_BLUE_BACKGROUND +
-													   "                        ♦     ");
-			System.out.println(Cores.TEXT_BLACK + "    |" + Cores.TEXT_YELLOW_BOLD +
-					           "               MICHAEL" + Cores.TEXT_PURPLE_BOLD + " ♦ " 
-			+ Cores.TEXT_YELLOW_BOLD + "BANK               " + Cores.TEXT_BLACK + "|    "
-			+ Cores.TEXT_RESET + Cores.TEXT_BLACK + Cores.ANSI_BLUE_BACKGROUND);
+			System.out.println(Cores.TEXT_BLUE_BRIGHT + Cores.ANSI_BLUE_BACKGROUND + "     ♦                  "
+					+ Cores.TEXT_RED_BRIGHT + Cores.ANSI_BLUE_BACKGROUND + "                        ♦     ");
+			System.out.println(Cores.TEXT_BLACK + "    |" + Cores.TEXT_YELLOW_BOLD + "               MICHAEL"
+					+ Cores.TEXT_PURPLE_BOLD + " ♦ " + Cores.TEXT_YELLOW_BOLD + "BANK               " + Cores.TEXT_BLACK
+					+ "|    " + Cores.TEXT_RESET + Cores.TEXT_BLACK + Cores.ANSI_BLUE_BACKGROUND);
 			System.out.println("    |         Nosso maior cuidado é você!        |    ");
 			System.out.println("    |____________________________________________|    ");
 			System.out.println("    |                                            |    ");
@@ -57,56 +60,156 @@ public class Menu {
 			System.out.println("                                                      ");
 			System.out.println("                                                      ");
 			System.out.printf("     Entre com a opção desejada:                     ");
-			opcao = leia.nextInt();
-			System.out.println("                                                      ");			
-				
+			try {
+				opcao = leia.nextInt();
+				System.out.println("                                                      ");
+			} catch (InputMismatchException e) {
+				System.out.println("                                                      ");
+				System.out.println("     Digite valores inteiros!                         ");
+				leia.nextLine();
+				opcao = 0;
+			}
+
 			if (opcao == 9) {
 				System.out.println(Cores.TEXT_YELLOW_BOLD + "     Obrigado por fazer parte!                        ");
 				System.out.println(Cores.TEXT_WHITE_BOLD + "     Michael Bank - Nosso maior cuidado é você!       ");
 				leia.close();
 				System.exit(0);
 			}
-				
-			switch (opcao) {
-				case 1:
-					System.out.println(Cores.TEXT_WHITE + "     Criar conta                                      ");
-				
-                    break;
-				case 2:
-					System.out.println(Cores.TEXT_WHITE + "     Listar todas as contas                           ");
-					
-                    break;
-				case 3:
-					System.out.println(Cores.TEXT_WHITE + "     Consultar dados da conta - Por número            ");
-	
-                    break;
-				case 4:
-					System.out.println(Cores.TEXT_WHITE + "     Atualizar dados da conta                         ");
-					
-                    break;
-				case 5:
-					System.out.println(Cores.TEXT_WHITE + "     Apagar a conta                                   ");
-		
-                    break;
-				case 6:
-					System.out.println(Cores.TEXT_WHITE + "     Saque                                            ");
 
-                    break;
-				case 7:
-					System.out.println(Cores.TEXT_WHITE + "     Depósito                                         ");
+			switch (opcao) {
+			case 1:
+				System.out.println(Cores.TEXT_WHITE + "     Criar conta                                      ");
+				
+				System.out.printf("     Digite o número da agência:                   ");
+				agencia = leia.nextInt(); 
+				System.out.printf("     Digite o nome do Titular:                        ");
+				leia.skip("\\R?");
+				titular = leia.nextLine();
+
+				do {
+					System.out.println("     Digite o tipo da conta (1 - CC ou 2 - CP):   ");
+					tipo = leia.nextInt();
+					} while (tipo < 1 && tipo > 2);
+
+					System.out.println("     Digite o saldo da conta (R$):                ");
+					saldo = leia.nextFloat();
+
+					switch (tipo) {
+						case 1:
+							System.out.println("     Digite o limite de crédito (R$):             ");
+							limite = leia.nextFloat();
+							contas.cadastrar(new ContaCorrente(contas.gerarNumero(), agencia, tipo, titular, saldo, limite));
+							break;
+						case 2: 
+							System.out.println("     Digite o dia do aniversario da conta:        ");
+							aniversario = leia.nextInt();
+							contas.cadastrar(
+							new ContaPoupanca(contas.gerarNumero(), agencia, tipo, titular, saldo, aniversario));
+							break;
+				}
+
+				keyPress();
+				break;
+			case 2:
+				System.out.println(Cores.TEXT_WHITE + "     Listar todas as contas                           ");
+				contas.listarTodas();
+				keyPress();
+				break;
+			case 3:
+				System.out.println(Cores.TEXT_WHITE + "     Consultar dados da conta - Por número            ");
+				
+				System.out.println("     Digite o número da conta: ");
+				numero = leia.nextInt();
+				
+				contas.procurarPorNumero(numero);
+
+				keyPress();
+				break;
+			case 4:
+				System.out.println(Cores.TEXT_WHITE + "     Atualizar dados da conta                         ");
+				
+				System.out.println("     Digite o número da conta: ");
+				numero = leia.nextInt();
+				
+				if(contas.buscarNaCollection(numero) != null) {
+					System.out.println("     Digite o número da agência: ");
+					agencia = leia.nextInt();
+					System.out.println("Digite o nome do titular: ");
+					leia.skip("\\R?");
+					titular = leia.nextLine();
 					
-                    break;
-				case 8:
-					System.out.println(Cores.TEXT_WHITE + "     Transferência entre contas                       ");
+					System.out.println("Digite o saldo da conta (R$): ");
+					saldo = leia.nextFloat();
 					
-                    break;
-				default:
-					System.out.println(Cores.TEXT_RED_BOLD + "     Opção inválida.                                  " + Cores.TEXT_RESET);
-                    break;
+					tipo = contas.retornaTipo(numero);
+					
+					switch(tipo) {
+					case 1:
+						System.out.println("     Digite o limite de crédito (R$): ");
+						limite = leia.nextFloat();
+						contas.atualizar(new ContaCorrente(numero, agencia, tipo, titular, saldo, limite));
+						break;
+					case 2:
+						System.out.println("     Digite o dia do aniversário da conta: ");
+						aniversario = leia.nextInt();
+						contas.atualizar(new ContaPoupanca(numero, agencia, tipo, titular, saldo, aniversario));
+						break;
+					default:
+						System.out.println("\n     Tipo de conta inválido.");
+					}
+				}else
+					System.out.println("\n     Conta não encontrada.");
+
+				keyPress();
+				break;
+			case 5:
+				System.out.println(Cores.TEXT_WHITE + "     Apagar a conta                                   ");
+				
+				System.out.println("     Digite o número da conta: ");
+				numero = leia.nextInt();
+				
+				contas.deletar(numero);
+
+				keyPress();
+				break;
+			case 6:
+				System.out.println(Cores.TEXT_WHITE + "     Saque                                            ");
+
+				keyPress();
+				break;
+			case 7:
+				System.out.println(Cores.TEXT_WHITE + "     Depósito                                         ");
+
+				keyPress();
+				break;
+			case 8:
+				System.out.println(Cores.TEXT_WHITE + "     Transferência entre contas                       ");
+
+				keyPress();
+				break;
+			default:
+				System.out.println(Cores.TEXT_RED_BOLD + "     Opção inválida.                                  "
+						+ Cores.TEXT_RESET);
+
+				keyPress();
+				break;
 			}
-			
-		}	
-		
+
+		}
+
 	}
 
+	public static void keyPress() {
+		try {
+			System.out.println("                                                      ");
+			System.out.println(Cores.ANSI_BLUE_BACKGROUND + Cores.TEXT_WHITE_BOLD
+					+ "     Pressione Enter para continuar.                  ");
+			System.in.read();
+
+		} catch (IOException e) {
+			System.out.println("                                                      ");
+			System.out.println("Você pressionou uma tecla diferente de Enter.         ");
+		}
+	}
 }
